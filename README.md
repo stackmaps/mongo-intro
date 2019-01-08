1c8c8eec720e1f3178257bfcb4134823ca13b181
Robyn C. Allen <robyninboston@gmail.com>
2019-01-07 19:37:26 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# mongo-intro
+
+Intermediate learning project introducing PyMongo, the Python driver for MongoDB.
+
+In this project, you will write your own database interface code.  An interface is simply a piece of code that serves as an intermediary between one piece of software and another (or between a piece of software and a human).  
+
+## Difficulty Level:  INTERMEDIATE
+
+You should be comfortable with Python, understanding functions, try/except statements, the `input()` function, and data types.  You should be willing to accept some abstraction, e.g., willing to not understand every single detail underneath the code you import.  You should be able to read documentation in order to learn how to install MongoDB and PyMongo on your computer.
+
+## The Story
+
+One day, in the very far future, books become a scarce commodity.  You decide to take it upon yourself to create a database instance which will enable you to digitize the characters from Charlotte's Web, thereby preserving the memory of this historic creative masterwork for all time.
+
+## What you will learn
+
+In this project, you will learn about:
+* MongoDB
+* differences between PyMongo and the Mongo Shell
+* CRUD operations
+
+## Links and Resources
+
+https://docs.mongodb.com/manual/installation/#tutorial-installation
+https://docs.mongodb.com/manual/tutorial/getting-started/
+https://api.mongodb.com/python/current/
+
+## License
+
+Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
---
+++ mongofarm.py
@@ -0,0 +1,103 @@
+""" mongofarm.py
+
+	This file presumes a running mongod instance.
+
+	Multiple learning TODO items are listed.
+
+	PyMongo is the Python driver for MongoDB.  
+	The PyMongo docs are here:
+	https://api.mongodb.com/python/current/
+"""
+
+from pymongo import MongoClient
+
+
+client = MongoClient("localhost", 27017)
+db = client["farm"]
+animals = client["animals"]
+print("we are using db {}".format(db.name))
+print("...and collection {}".format(db.animals.name))
+
+choice = input("\nWant to add a document? Y/n > ")
+adding_data = (True if "Y" == choice else False)
+
+if adding_data:
+	db.animals.insert_one({
+		"name": "Charlotte",
+		"type": "spider",
+		"age": 100,
+		"favorite_foods": ["aphids","moths","flies"]})
+
+print("\nLooking for data...\n")
+animal = db.animals.find_one()  # returns a document or None
+# my_query = {"name": "Charlotte"}
+# print(my_query)
+# animal = db.animals.find_one(my_query)
+print(animal)
+
+
+print("\nRetrieving a specific field from all documents....\n")
+
+for item in db.animals.find():
+    print(item["favorite_foods"])
+
+
+# TODO 0
+# Read this entire file and add comments explaining each line.
+# BEFORE you run the file, open the mongo shell and run this command:
+#     		> show dbs
+
+# Does the `farm` database exist yet?
+
+
+# TODO 1
+# Run the file!    
+# Use the mongo shell to go see what's in the database.
+#			 > use farm
+#			 > db.animals.findOne()
+
+
+# TODO 2
+# Un-comment lines 31-33.  Run the file.  
+# Change the data loading code to add a different farm animal.
+# Change the query on line 31 to retrieve the different farm animal.
+# Have your new query use the `age` or `type` key (not `name`).
+
+
+# TODO 3
+# Try adding some data of a different format to this exact same db.
+# What happens?  Does MongoDB crash?
+
+
+# TODO 4
+# Write a function which adds animals to the farm.  
+# (specifically, it adds documents to the `animals` collection
+# inside the `farm` database)
+# 		What's the advantage of having a function?
+
+
+# TODO 5
+# Write a function which takes a dictionary as input
+# and which enforces a schema on that dictionary.
+# Return `True` if the dictionary has the right schema;
+# return `False` otherwise.
+
+
+# TODO 6
+# Write a function which loads data from a CSV into MongoDB.
+# Use the function you wrote in TODO 5 in order to enforce a schema.
+# Feel free to continue using the Charlotte's Web example
+# or devise a new example. 
+# Make sure you provide human-readable error messages.
+
+
+# TODO 7
+# Start a standalone app, in a new file, unrelated to this one.
+# Your program should be an interface to a MongoDB database.
+# The program should first ask for a user ID.
+# The program should serve a series of survey questions.
+# The program should store survey responses in a MongoDB database.
+# Respondents can opt-out of the survey partway through.
+# New respondents must be prompted for a user ID.
+
+print("\nDone.")
